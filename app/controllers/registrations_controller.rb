257ffac7c95ec810a2a214f2623b3fac0061a223
ejaramillom <ejaramilo@hotmail.com>
2024-08-10 21:16:42 -0500

    feat(user_registration): Add mailer config in environment and letter opener

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,15 @@
 class RegistrationsController < ApplicationController
+  def create
+    user = User.new(registration_params)
+    if user.save
+      render json: { message: 'User created successfully' }, status: :created
+    else
+      render json: {errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def registration_params
+    params.require(:user).permit(:email, :username, :password)
+  end
 end
